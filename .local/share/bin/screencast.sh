8c9007fb70697fb0ff34cd5eb4e123e31acccd58
Sam Erickson <samericksn@gmail.com>
2019-06-14 08:02:36 -0700

    Changed the way screencast operate

--- .local/share/bin/screencast.sh
+++ .local/share/bin/screencast.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
 # This script starts a screencast
+DISPLAY="$(~/.local/share/bin/select_focused.sh)"
+
+[ "$#" -ne 1 ] && printf "Expected an argument.\n"
+
+rec-status() {
+	[ "$(pidof wf-recorder)" ] && \
+		printf "<span color=\"#f26d6d\">RECORDING ACTIVE</span>" || \
+		printf "<span color=\"#91378b\">recording inactive</span>"
+	exit 0
+}
+
+rec-start() {
+	[ "$(pidof wf-recorder)" ] && \
+		notify-send -t 1500 "Error: screencast already running" && exit 1
 	
+	notify-send -t 1500 "Starting screencast on $DISPLAY"
+	wf-recorder -o "$DISPLAY" -f "$HOME/Videos/screencast-$(date +%Y%m%d%h%m).mp4"
+}
 
-
-DISPLAY="$(~/.bin/select_focused.sh)"
-
-[ ! -z $(pidof wf-recorder) ] && \
-	notify-send -t 1500 "Error: screencast already running" && \
-	exit 1 
-
-notify-send -t 1500 "Starting screencast on $DISPLAY" && \
-	wf-recorder -o $DISPLAY ~/Videos/screencast-$(date +%Y%m%d%h%m).mkv && \
+rec-stop() {
+	[ ! "$(pidof wf-recorder)" ] && \
+		notify-send -t 1500 "Error: no active screencast to terminate" && exit 1
+	
+	kill -2 $(pidof wf-recorder)
 	notify-send -t 1500 "Stoped screencast"
+	exit 0
+
+}
+
+# Validate user input
+if [ "$1" = "start" ]; then
+	rec-start && exit 0 || exit 1
+elif [ "$1" = "stop" ]; then
+	rec-stop && exit 0 || exit 1
+elif [ "$1" = "status" ]; then 
+	rec-status && exit 0 || exit 1
+else
+	printf "ERROR: \"$1\" is not a valid argument.\n" && exit 1
+fi
+
