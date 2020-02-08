b077ee7ebe7bfc094b725e0ba15b891bee38ddca
Sam Erickson <sam@samerickson.me>
2020-02-07 20:57:06 -0800

    Updated statusbar scripts

--- .config/i3blocks/scripts/disk.sh
+++ .config/i3blocks/scripts/disk.sh
@@ -1,15 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is used to display disk usage information in a status bar
-
 usage="$(df -h "$1" | awk ' /[0-9]/ {print $3 "/" $2}')"
-value="$(echo "scale=2;$usage*100" | bc | cut -c1-2 )"
 
-if [ "$value" -gt "95" ]; then
-	color="$RED"
-elif [ "$value" -gt "90" ]; then
-	color="$YELLOW"
-else
-	color="$LIME"
-fi
-printf "<span color='%s'>%s: </span><span color='%s'>%s</span>\n" "$YELLOW" "$1" "$color" "$usage"
+printf "<span color='%s'>%s: </span><span color='%s'>%s</span>\n" "$YELLOW" "$1" "$MAGENTA" "$usage"
