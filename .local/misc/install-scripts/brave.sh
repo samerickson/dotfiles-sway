ab36d708a007845f6c5dec9b3b07a7a107887ee1
Sam Erickson <sam@samerickson.me>
2020-02-07 19:13:56 -0800

    Cleaned up files

--- .local/misc/install-scripts/brave.sh
+++
@@ -1,7 +0,0 @@
-
-# If you get an issue with userspace not being enabled, run:
-#		sudo sysctl kernel.unprivileged_userns_clone=1
-
-git clone https://aur.archlinux.org/brave-bin.git
-cd brave-bin
-makepkg -si
--- .local/misc/install-scripts/yay.sh
+++
@@ -1,4 +0,0 @@
-git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay
-makepkg -si
-rm -r /tmp/yay
